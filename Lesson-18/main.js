f439ec882ef37bb5b91f356c32a8544ae90b8219
Pasha Arlou <pashaarlou@gmail.com>
2021-10-06 08:07:45 +0300

    Homework 18

---
+++ Lesson-18/main.js
@@ -0,0 +1,49 @@
+'use strict';
+// Задание 1:
+var userEmail = 'name_surname-1234@gmail.com';
+var reg = /^[a-z]{3,10}_[a-z]{3,10}(-\d{4})?@([^\W_]{2,10}(\.|-)?)[^\W_]{2,10}\.com$/i;
+console.log(reg.test(userEmail));
+
+// Задание 2:
+var userPhoneNamber1 = '+375-25-777-77-77';
+var userPhoneNamber2 = '375299999999';
+var userPhoneNamber3 = '8-044-444-44-44';
+var userPhoneNamber4 = '8033-6666666';
+
+function valiNamber(number) {
+	var reg = /^\+?\d{1,3}-?0?(25|44|33|17|29)-?[^\D0]\d{2}(-?\d{2}){2}$/i;
+	if (reg.test(number)) return true;
+	return false;
+}
+console.log(valiNamber(userPhoneNamber1));
+console.log(valiNamber(userPhoneNamber2));
+console.log(valiNamber(userPhoneNamber3));
+console.log(valiNamber(userPhoneNamber4));
+
+// Задание 3:
+var anyWords = prompt('Введите любое слово', '');
+function volwesCount(word) {
+	var count = 0;
+	var reg = /[aeiouауоыиэяюёе]/i;
+	word.split('').forEach(function (element) {
+		if (reg.test(element)) count++;
+	});
+	return count;
+}
+console.log(`количество гласных букв в слове = ${volwesCount(anyWords)}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Задание 3:
+// Переписать решение задачи с поиском гласных с использованием регулярного выражения. Протестировать ситуацию, когда
+// гласных в переданном тексте будет 0. По возможности придумать несколько вариантов решения.
